0e3b590898841e78645daa794579ab20e4ef4de6
noori <amiralinoori0688@gmail.com>
2025-07-13 17:06:12 +0330

    Add files via upload

---
+++ Main_p[Beep_Program].cs
@@ -0,0 +1,23 @@
+﻿namespace Beep_Program
+{
+    internal class Beep_Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("*************************made by nooriplayer2024**********************");
+            while (true)
+            {
+                count++;
+                Console.WriteLine("-Beep");
+                Console.Beep();
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey();
+                    Console.WriteLine("-Beep is stopped" + "\n-beep count is [" + count + "]");
+                    break;
+                }
+            }
+        }
+    }
+}
